34c2e5232fbd8e722a1d9453d26d59d6f1a4cdb8
Vaqif <taqiyevv937@gmail.com>
2022-09-20 03:58:00 +0400

    new file add

---
+++ src/main/kotlin/IcerikDegistirme.kt
@@ -0,0 +1,18 @@
+fun main(){
+    val numbers = ArrayList<Int>()
+
+    numbers.add(5)
+    numbers.add(10)
+    numbers.add(15)
+
+    for (i in numbers){
+        println(i * 2)
+    }
+
+
+
+
+
+
+
+}
